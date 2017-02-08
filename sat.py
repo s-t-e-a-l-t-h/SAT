619822a511bbaab00ef7eb5e8f212e6b02d2596f
Mike <mikecokina@gmail.com>
2017-02-08 11:08:42 +0100

    Add files via upload

--- sat.py
+++ sat.py
@@ -1,6 +1,6 @@
 # implementation of SAP algorithm in 2d
-# detection of collision of two triangles
-# if triangles are touched, return False too
+# detection of collision of two faces
+# if faces are touched, return False too
 
 import Plot as Plt
 import numpy as np
@@ -9,6 +9,7 @@ import numpy as np
 
 # round to
 PRECISION = 10
+
 
 def projection(v, t):
     t, v = np.array(t), np.array(v)
@@ -22,8 +23,11 @@ def to_tangential(tn, nn, pt):
 
 
 def separating_axis_theorem(tri1, tri2):
-    # each edge of triangle tri1 and tri2
-    edges = np.array([[tri1[i], tri1[i + 1]] for i in range(-1, 2)] + [[tri2[i], tri2[i + 1]] for i in range(-1, 2)])
+    # each edge of face tri1 and tri2
+    # number of face vertices
+    nfv = len(tri1)
+    edges = np.array(
+        [[tri1[i], tri1[i + 1]] for i in range(-1, nfv - 1)] + [[tri2[i], tri2[i + 1]] for i in range(-1, nfv - 1)])
 
     for edge in edges:
         # tangent vector of testing line
@@ -31,7 +35,7 @@ def separating_axis_theorem(tri1, tri2):
         # normal vector of testing line
         normal = -tangent[1], tangent[0]
 
-        # we flip tangent and normal vector (we will project all points of each triangle on the normal line of
+        # we flip tangent and normal vector (we will project all points of each face on the normal line of
         # current edge)
         tangent, normal = normal, tangent
 
@@ -42,7 +46,7 @@ def separating_axis_theorem(tri1, tri2):
         # new x coo (y should be zero now)
         projection_t1_newx, projection_t2_newx = list(zip(*projection_t1))[0], list(zip(*projection_t2))[0]
 
-        # maximal length projected of each triangle
+        # maximal length projected of each face
         projection_edge1, projection_edge2 = [round(min(projection_t1_newx), PRECISION),
                                               round(max(projection_t1_newx), PRECISION)], \
                                              [round(min(projection_t2_newx), PRECISION),
@@ -56,6 +60,7 @@ def separating_axis_theorem(tri1, tri2):
             return True
 
     return False
+
 
 def intersection(t1, t2):
     return not separating_axis_theorem(t1, t2)
@@ -161,16 +166,35 @@ def intersection(t1, t2):
 #             face_alpha=0.5, azim=-90, elev=90)
 
 
-faces = [[[-0.4760028, 0.37223806],
-          [-0.33617698, 0.04619783],
-          [-0.19737227, 0.31664461]],
-         [[0., 0.],
-          [-0.33617698, 0.04619783],
-          [-0.19737227, 0.31664461]]]
+# faces = [[[-0.4760028, 0.37223806],
+#           [-0.33617698, 0.04619783],
+#           [-0.19737227, 0.31664461]],
+#          [[0., 0.],
+#           [-0.33617698, 0.04619783],
+#           [-0.19737227, 0.31664461]]]
+#
+# print(intersection(faces[0], faces[1]))
+# for i in range(0, 2):
+#     for j in range(0, 3):
+#         faces[i][j].append(0.0)
+# Plt.plot_3d(faces=[faces], faces_view=True, normals_view=False, points_view=False, face_color=[["r", "g"]],
+#             face_alpha=0.5, azim=-90, elev=90)
 
-print(intersection(faces[0], faces[1]))
-for i in range(0, 2):
-    for j in range(0, 3):
-        faces[i][j].append(0.0)
-Plt.plot_3d(faces=[faces], faces_view=True, normals_view=False, points_view=False, face_color=[["r", "g"]],
-            face_alpha=0.5, azim=-90, elev=90)
+
+# # 4 dim faces:
+# faces = [[[0.0, 0.5],
+#           [1.0, 0.5],
+#           [1.0, 1.0],
+#           [0.0, 1.0]],
+#          [[0.5, 0.5],
+#           [4.0, 0.5],
+#           [4.0, 3.0],
+#           [0.5, 3.0]]]
+# print(intersection(faces[0], faces[1]))
+# for i in range(0, 2):
+#     for j in range(0, 4):
+#         faces[i][j].append(0.0)
+# Plt.plot_3d(faces=[faces], faces_view=True, normals_view=False, points_view=False, face_color=[["r", "g"]],
+#             face_alpha=0.5, azim=-90, elev=90)
+
+
